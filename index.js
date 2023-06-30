089486a287dde9774571b923aa4d924176377aeb
Dave Honneffer <pearofducks@gmail.com>
2023-06-30 10:45:52 +0200

    Yep

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 import { presetWarp } from '@warp-ds/uno';
 import { createAutocomplete } from '@unocss/autocomplete';
 import { createGenerator } from "@unocss/core";
+import fabricClasses from './fabric-classes.js'
+import { classes as warpComponents } from '@warp-ds/component-classes/classes'
+
+export const classes = [...fabricClasses.map(c => c.substring(1)), ...warpComponents]
+
+const semanticMap = {
+  'color-icon': 'icon',
+  'color-text': 'text',
+  'color-border': 'border',
+  'color-background': 'bg'
+}
 
 const tokens = await (await fetch('https://assets.finn.no/pkg/@warp-ds/tokens/v1/finn-no.css')).text()
-console.log(tokens)
-console.log(tokens.split(';').filter(t => t.startsWith('--w-s')))
-//
-// const getAllWarpClasses = async () => {
-//   const uno = createGenerator({ presets: [presetWarp({ skipPreflight: true })] })
-//   const ac = createAutocomplete(uno);
-//   return Array.from(await ac.enumerate());
-// }
-//
-// const f = await getAllWarpClasses()
-// console.log(f.filter(e => !e.endsWith(':')).join('\n'))
+const interestingTokens = tokens.split(';').filter(t => t.startsWith('--w-s')).map(t => t.split(':')[0])
+for (const [k, v] of Object.entries(semanticMap)) {
+  const r = interestingTokens.filter(t => t.includes(k)).map(t => t.replace(`--w-s-`, '').replace(k, v))
+  classes.push(...r)
+}
+const getAllWarpClasses = async () => {
+  const uno = createGenerator({ presets: [presetWarp({ skipPreflight: true })] })
+  const ac = createAutocomplete(uno);
+  return Array.from(await ac.enumerate());
+}
+const f = await getAllWarpClasses()
+const filtered = f.filter(e => !e.match(/[^a-z-]/)).join('\n')
+classes.push(...filtered)
+
+// const uno = createGenerator({ presets: [presetWarp({ skipPreflight: true })] })
+// const results = await uno.generate(classes)
+// console.log(results.matched.size)
